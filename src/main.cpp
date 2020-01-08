38d024db8f4e7503984ec157238f25271f28ccf5
Paul <paul.bertrand@eleves.ec-nantes.fr>
2020-01-08 11:34:55 +0100

    Ajout de la structure principale

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+
+int main() {
+
+  /** Boucle affichage des images des caméras */
+
+  // Image Avant
+
+  // Image arrière
+
+  // Traitement des images
+
+  // Image avant traitée
+
+  // Image arrière traitée
+  return 0;
+}
