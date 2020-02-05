f8bff25a00bc896ece2af4f165af5df5150281ea
Benjamin <ben.heinrich@live.fr>
2020-02-05 17:46:14 +0100

    Déplace cimage fonctionnel. A factoriser

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <vector>
+#include "windows.h" //j'etais obligé désopasdéso (il n'y a pas de multiplateforme pour l'appui touche)
 // namespace
 using namespace std;
 using namespace cv;
@@ -16,7 +17,7 @@ using namespace cv;
 // Default wdth and height of the video
 #define DEFAULT_VIDEO_WIDTH   800
 #define DEFAULT_VIDEO_HEIGHT  600
-#define pixelParMetre 0.000264 ///http://www.yourwebsite.fr/index.php/documents/287-relation-entre-pixel-et-taille-s-des-images
+#define pixelParMetre 0.000311 ///http://www.yourwebsite.fr/index.php/documents/287-relation-entre-pixel-et-taille-s-des-images
 // Defining escape key
 #define KEY_ESCAPE 27
 
@@ -45,25 +46,89 @@ int main(){
 		// on joue la première image
 		cap >> curImg;
 	}
+
+
+
+	//! on récupère la largeur et hauteur de l'image
+	int widthFrame  = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	int heightFrame = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	std::cout<<"Frame width = "<<widthFrame<<std::endl;
+	std::cout<<"Frame height = "<<heightFrame<<std::endl;
+
+
 	//! création des fenetres OpenCV
 	//! Ce que renvoit la caméra
 	string windowNameCapture = "Image obtenue par la caméra";
 	namedWindow(windowNameCapture, CV_WINDOW_AUTOSIZE);
 	imshow(windowNameCapture, curImg);
+
+	//! L'image découpée effet fenêtre
+	string windowNameCapture2 = "Fenêtre";
+	namedWindow(windowNameCapture2, CV_WINDOW_AUTOSIZE);
+	imshow(windowNameCapture2, curImg);
 	
+	//! L'image découpée effet fenêtre
+	string windowNameCapture3 = "Fenêtre avec mouvement de la tête";
+	namedWindow(windowNameCapture3, CV_WINDOW_AUTOSIZE);
+	imshow(windowNameCapture3, curImg);
+
+
+	//! on chosit la fenetre qu'on veut
+	Range decoupageColonne(heightFrame/2-150, heightFrame/2+150);
+	Range decoupageLigne(widthFrame/2-100,widthFrame/2+100);
+	
+
+	float compteur=0;
+	//! on récupère la position de la tête relative a la camera (ici elle est fixée) 
+	float positionTete[3];
+	positionTete[0]= 0;
+	positionTete[1]= 0.5;
+	positionTete[2]= 0;
+
+
+	//! on change l'image obtenue
+	//! d'après de longs calculs (thalès), l'image obtenue dépend de la distance de la caméra au "paysage"...
+	//! On va donc récupérer la taille du marqueur à chaque frame, le comparer a sa taille réelle et en déduire ma distance caméra paysage
+	//! Pour l'instant sans marqueur on suppose la distance = 50 cm
+
+
+
+
+
+
+	decalagePixelHorizontal = distanceCameraPaysage * positionTete[0]/positionTete[1]/pixelParMetre;
+	decalagePixelVertical =  distanceCameraPaysage * positionTete[2]/positionTete[1]/pixelParMetre;
 
 	//! on l'affiche en boucle !
 	while(key!= KEY_ESCAPE){
 
-		cap>>curImg;
+		compteur+=0.1;
+		cap >> curImg;
 		imshow(windowNameCapture, curImg);
 
 
+		//! on coupe l'image pour qu'elle soit de la taille de la fenetre (position de la caméra en haut à droite de l'écran)
+		imgFen = curImg(decoupageLigne,decoupageColonne);
 
-		//! on récupère la position de la tête relative a la camera (ici elle est fixée) 
 		
-		//float positionTete[3]=...
+		//! on l'affiche
+		imshow(windowNameCapture2, imgFen);
+
+
+
+
+		if (GetKeyState(VK_UP)& 0x8000/*Check if high-order bit is set (1 << 15)*/) decalagePixelVertical++;
+		if (GetKeyState(VK_DOWN)& 0x8000/*Check if high-order bit is set (1 << 15)*/) decalagePixelVertical--;
+		if (GetKeyState(VK_LEFT)& 0x8000/*Check if high-order bit is set (1 << 15)*/) decalagePixelHorizontal++;
+		if (GetKeyState(VK_RIGHT)& 0x8000/*Check if high-order bit is set (1 << 15)*/) decalagePixelHorizontal--;
+
+		Range decoupageLigne2(decoupageLigne.start+decalagePixelVertical,decoupageLigne.end+decalagePixelVertical);
+		Range decoupageColonne2(decoupageColonne.start+decalagePixelHorizontal,decoupageColonne.end+decalagePixelHorizontal);
 		
+		//! on coupe l'image et on l'affiche
+		imgFen2 = * new Mat(heightFrame,widthFrame, 0);
+		imgFen2 = curImg(decoupageLigne2,decoupageColonne2);
+		imshow(windowNameCapture3, imgFen2);
 		
 
 		key = waitKey(1);
